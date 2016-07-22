cc78b016de5ff7d3f0743b66639f9506ba6a6c8f
ishwar <ishwar.tiwari@kelltontech.com>
2016-07-22 19:43:29 +0530

    Image widget selector for Ckeditor

---
+++ README.md
@@ -0,0 +1,56 @@
+### Image Widget Selector
+
+This repository is meant to provide Image Widget Selector for [Ckeditor](http://ckeditor.com/) Plugin repository on GitHub.
+
+This module have been developed for Ckeditor.
+
+This module of ckeditor will create a select link  in ckeditor. 
+
+To use this module you need to download ckeditor plugin  . [Download keditor](http://ckeditor.com/).
+
+You need to copy and paste this module in ckeditor plugin folder and after doing this will create a image widget selector icon.
+
+
+
+### What is the puspose of this module
+Once you use this plugin and icon will be available for Image Widget Selector, after clicking on this icon a popup will be opened.In popup there will be third party website means you can run another website in popup and if you want to select and image then you can select and that selected image will be visiable in editor, popup will be closed.
+
+
+### Required settings 
+1. There are some variable in plugin are hardcoaded so you need to set first set the variables. 
+
+   Update plugin.js
+    
+   var iframeWindow = null;
+   var imagewidgetwindowurl = '<<<THIRDPARTY_WINDOW_URL>>>';
+   var imagewidgetwindow_host_child = '<<<THIRDPARTY_WINDOW_URL_HOSTURL>>>';
+   var imagewidgetwindow_host_parent = '<<<THIRDPARTY_WINDOW_URL_PARENT_URL>>>';
+
+
+2. You need to add some code in you third party website and this added code will be the responsible for sending the selected image to the parent website that will be further added in Ckeditor.
+
+
+     function sendToParent() {
+                var file_path = <YOUR_IMAGE_PATH>; // This will be dynamic
+                var ckeditorInstance = getParamator('CKEditor');
+                var mvreplacetype = getParamator('mvreplacetype');
+                var mvfolderpath = getParamator('mvfolderpath');
+                var actual_data = file_path + '||||' + ckeditorInstance + '||||' + mvreplacetype+ '||||' + mvfolderpath;
+                window.parent.postMessage(actual_data, '*');
+            }
+
+ 3. Allow Cross Domain 
+    Option  1 .  header("Access-Control-Allow-Origin: http://yourdomain-you-are-connecting-from.com"); // For PHP
+    Option  2 .  In Apache use this to allow cross domain 
+                  Header set Access-Control-Allow-Origin "*"            
+                  [Refer this link](http://enable-cors.org/server_apache.html)
+
+
+
+
+
+
+### Note: This module will work only for Ckeditor
+
+Thanks! :Ishwar Chandra Tiwari:
+
---
+++ plugin.js
@@ -0,0 +1,132 @@
+﻿/**
+ * @license Copyright (c) Image Widget, CKSource - Ishwar Chandra. All rights reserved.
+ * For licensing, see LICENSE.md or http://ckeditor.com/license
+ */
+
+        /**
+         * @fileOverview The "imagewidgetselector" plugin that makes it possible to upload
+         *               and reflect image to editor.
+         *
+         */
+
+
+
+var iframeWindow = null;
+var imagewidgetwindowurl = 'http://drupal7.local/mvalet.php';
+var imagewidgetwindow_host_child = 'http://drupal7.local';
+var imagewidgetwindow_host_parent = 'http://drupal8.local';
+
+var win;
+var dialog;
+var imagewidget_img_counter = 0;
+var mvreplacetype = 'd8ckeditor';
+
+CKEDITOR.plugins.add('imagewidgetselector', {
+    icons: 'imagewidgetselector',
+    hidpi: true,
+    init: function (editor)
+    {
+        // Code start for window
+        editor.ui.addButton('ImagewidgetButton', {
+            label: Drupal.t('ImagewidgetButton'),
+            command: 'imagewidgetselector',
+            icon: this.path + 'icons/icon.png',
+            click: function (editor) {
+
+                try {
+                    CKEDITOR.currentInstance.name;
+                    if (imagewidget_img_counter <= 1)
+                        imagewidget_img_counter = imagewidget_img_counter + 1;
+                } catch (e) {
+                    imagewidget_img_counter = 0;
+                    return false;
+                }
+
+
+                $ = jQuery;
+                var iframe = $('<iframe id="imagewidgetframe" frameborder="0" marginwidth="0" marginheight="0" allowfullscreen></iframe>');
+                dialog = $("<div></div>").append(iframe).appendTo("body").dialog({
+                    autoOpen: false,
+                    modal: true,
+                    resizable: false,
+                    width: "auto",
+                    height: "auto",
+                    close: function () {
+                        iframe.attr("src", "");
+                    }
+                });
+                var src = imagewidgetwindowurl + '?CKEditor=' + CKEDITOR.currentInstance.name + '&amp;imagewidgetwindow_host_parent=' + imagewidgetwindow_host_parent + '&mvreplacetype=' + mvreplacetype;
+                var title = 'Image Widget integration';
+                var width = 712;
+                var height = 514;
+                iframe.attr({
+                    width: +width,
+                    height: +height,
+                    src: src
+                });
+                dialog.dialog("option", "title", title).dialog("open");
+
+                // window.removeEventListener('message', receiveMessage);
+
+                window.addEventListener('message', receiveMessage, false);
+
+                function receiveMessage(evt)
+                {
+
+                    if (evt.origin === imagewidgetwindow_host_child)
+                    {
+
+                        var actualdata = evt.data;
+                        var actualdataArray;
+                        actualdataArray = actualdata.split('||||');
+                        if (actualdataArray[2] == mvreplacetype) {
+                            InsertHTML(actualdataArray[0], actualdataArray[1]);
+                        }
+                    }
+                }
+
+
+            }
+        });
+
+    }
+});
+
+
+function InsertHTML(file_path, instances)
+{  
+
+   
+var oEditor = CKEDITOR.instances[instances];
+    var value = file_path;
+    // Check the active editing mode.
+    if (oEditor.mode == 'wysiwyg')
+    {
+   
+        if (imagewidget_img_counter == 1) {
+            dialog_pop = $("<div>Please wait....</div>").appendTo("body").dialog({
+                autoOpen: false,
+                modal: true,
+                resizable: false,
+                width: "auto",
+                height: "auto",
+            });
+            var title = 'Image Widget Message';
+            
+                    imagewidget_img_counter = 0;
+                    dialog_pop.dialog("option", "title", 'Please wait...').dialog("close"); 
+            
+            
+            dialog_pop.dialog("option", "title", title).dialog("open");
+
+             oEditor.insertHtml('<img src="' + file_path + '" />');
+                    dialog_pop.dialog("option", "title", 'Please wait...').dialog("close");
+
+            imagewidget_img_counter = 0;
+        }
+        // win.close();
+    } else
+        alert('You must be on WYSIWYG mode!');
+    dialog.dialog("option", "title", 'sdsadasdasd').dialog("close");
+
+}
